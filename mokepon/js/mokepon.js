ebbcadffef21511c442d74bb2d3cbe9880b06391
Alejandro Cortes <macortesmolina@gmail.com>
2022-08-15 18:09:23 -0500

    Clases 18, 19, 20

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,26 @@
-function selecccionarMascotaJugador(){
-    alert("SELECCIONASTE TU MASCOTA!!")
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click", selecccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById("boton-mascota")
-botonMascotaJugador.addEventListener("click", selecccionarMascotaJugador)
+function selecccionarMascotaJugador(){
+    let inputHipodoge = document.getElementById("hipodoge")
+    let inputCapipepo = document.getElementById("capipepo")
+    let inputRatigueya = document.getElementById("ratigueya")
+
+    if (inputHipodoge.checked){
+        alert("Seleccionaste a Hipodoge!!!")
+    }else if (inputCapipepo.checked){
+        alert("Seleccionaste a Capipepo!!!")
+    }else if (inputRatigueya.checked){
+        alert("Seleccionaste a Ratigueya!!!")
+    }else{
+        alert("Olvidaste seleccionar a tu mascota 😢")
+    }
+        
+        
+
+
+}
+
+window.addEventListener("load", iniciarJuego)
